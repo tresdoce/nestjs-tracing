acdaff026f7bc640a1b8b80e8fc5a005ab9d77d3
Christian Calderon <ccalderon9411@hotmail.com>
2022-02-09 13:59:29 -0500

    chore(tracing): add export of sdk and configuration interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { TracingModule } from './tracing/tracing.module';
 export { TracingService } from './tracing/services/tracing.service';
 export { TracingInterceptor } from './tracing/interceptors/tracing.interceptor';
+export { OpenTelemetryConfiguration } from './tracing/interfaces/tracing.interface';
+export { otelSDK } from './tracing/constants/open-telemetry.constants';
